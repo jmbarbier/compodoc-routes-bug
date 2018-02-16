0360422b13b777b29c5d30c8974770aadc37eb21
BARBIER Jean-Matthieu <jm.barbier@solidev.net>
2018-02-16 10:22:45 +0100

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from "@angular/router";
 
 
 import { AppComponent } from './app.component';
+import { TestRouteComponent } from './test-route/test-route.component';
 
+const MAIN_ROUTES: Routes = [{
+    path: "test", component: TestRouteComponent
+}];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestRouteComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(MAIN_ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
